78a5bd5d51480fb9173766b6c80ad5d4edb9badc
Trever Johnston <treverj1029@gmail.com>
2019-09-22 14:31:42 -0600

    Testing out some analytics stuff

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,14 +4,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: '/account', component: () => import('pages/Account.vue') },
-      { path: '/about', component: () => import('pages/About.vue') },
-      { path: '/services', component: () => import('pages/Services.vue') },
-      { path: '/litigation', component: () => import('pages/Litigation.vue') },
-      { path: '/contact', component: () => import('pages/Contact.vue') },
-      { path: '/reviews', component: () => import('pages/Reviews.vue') },
-      { path: '/admin', component: () => import('pages/Admin.vue') },
+      { path: '', name: 'Home', component: () => import('pages/Index.vue') },
+      { path: '/account', name: 'Account', component: () => import('pages/Account.vue') },
+      { path: '/about', name: 'About', component: () => import('pages/About.vue') },
+      { path: '/services', name: 'Print/Copy Services', component: () => import('pages/Services.vue') },
+      { path: '/litigation', name: 'Litigation Services', component: () => import('pages/Litigation.vue') },
+      { path: '/contact', name: 'Contact', component: () => import('pages/Contact.vue') },
+      { path: '/reviews', name: 'Reviews', component: () => import('pages/Reviews.vue') },
+      { path: '/admin', name: 'Admin', component: () => import('pages/Admin.vue') },
     ]
   }
 ]
@@ -20,6 +20,7 @@ const routes = [
 if (process.env.MODE !== 'ssr') {
   routes.push({
     path: '*',
+    name: 'Not Found',
     component: () => import('pages/Error404.vue')
   })
 }
